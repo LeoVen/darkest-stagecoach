0574e38c8cbeafc5e211d40df3cb36e862a00858
LeoVen <leonardo.vencovsky@gmail.com>
2021-06-08 14:24:41 -0300

    Added GitHub link

--- darkest-stagecoach/src/App.tsx
+++ darkest-stagecoach/src/App.tsx
@@ -1,10 +1,13 @@
+import React from 'react'
 import {
+    Button,
     createMuiTheme,
     Paper,
     ThemeProvider,
     Typography
 } from '@material-ui/core'
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import './App.css'
 import { AllClasses } from './data'
 
@@ -14,7 +17,7 @@ const theme = createMuiTheme({
             fontFamily: 'Mate SC',
             fontSize: '2em',
             color: '#CCCCCC',
-            margin: '0.5em',
+            marginRight: '1em'
         },
         h2: {
             fontFamily: 'Mate SC',
@@ -24,9 +27,8 @@ const theme = createMuiTheme({
         subtitle1: {
             fontFamily: 'Mate SC',
             color: '#CCCCCC',
-            margin: '1em',
-            wordWrap: 'break-word',
-        },
+            wordWrap: 'break-word'
+        }
     },
     palette: {
         background: {
@@ -40,23 +42,55 @@ function App() {
     return (
         <ThemeProvider theme={theme}>
             <div style={{ backgroundColor: theme.palette.background.default }}>
-                <div style={{display: 'flex', alignItems: 'baseline', flexWrap: 'wrap'}}>
-                    <Typography variant='h1'>
-                        Darkest Stagecoach
-                    </Typography>
-                    <Typography variant='subtitle1'>
-                        A place to explore class mods for the <a href="https://www.darkestdungeon.com/">Darkest Dungeon</a> game
-                    </Typography>
-                </div>
+                <header
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        padding: '1em'
+                    }}>
+                    <div
+                        style={{
+                            display: 'flex',
+                            alignItems: 'baseline',
+                            flexWrap: 'wrap'
+                        }}>
+                        <Typography variant="h1">Darkest Stagecoach</Typography>
+                        <Typography variant="subtitle1">
+                            A place to explore class mods for the{' '}
+                            <a href="https://www.darkestdungeon.com/">
+                                Darkest Dungeon
+                            </a>{' '}
+                            game
+                        </Typography>
+                    </div>
+                    <div>
+                        <a href="https://github.com/LeoVen/darkest-stagecoach"
+                           target='_blank'>
+                            <FontAwesomeIcon icon={faGithub} size={'2x'} />
+                        </a>
+                    </div>
+                </header>
                 <div style={{ display: 'flex', flexWrap: 'wrap' }}>
                     {AllClasses.map(hero => {
                         return (
                             <Paper
                                 key={hero.name}
                                 elevation={5}
-                                style={{ margin: '1em', padding: '1em', display: 'flex', alignItems: 'center', width: '300px' }}>
-                                <img src={hero.image} style={{margin: '1em', maxWidth: 'calc(100vw - 6em)'}}/>
-                                <Typography variant='h2'>
+                                style={{
+                                    margin: '1em',
+                                    padding: '1em',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    width: '300px'
+                                }}>
+                                <img
+                                    src={hero.image}
+                                    style={{
+                                        margin: '1em',
+                                        maxWidth: 'calc(100vw - 6em)'
+                                    }}
+                                />
+                                <Typography variant="h2">
                                     {hero.name}
                                 </Typography>
                             </Paper>
